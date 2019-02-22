013d133391337a9f6945f526f2e823567c0bcbd1
VIGNESH SUBRAMANI <subramani.vignesh@jgc.com>
2019-02-22 13:01:01 +0900

    Docker sample

---
+++ ConsoleApp_DotNetCore/ConsoleApp_DotNetCore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp_DotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine(GetCharacter());
+            }
+        }
+
+        private static char GetCharacter()
+        {
+            const string chars= @"qwertyuiopasdfghjklzxcvbnm1234567890";
+            var random = new Random();
+            var num = random.Next(0, chars.Length - 1);
+            return chars[num];
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+1. Create a Sample Console Application (.NetCore) in VS2017
+2. Go to Cmd --> open project location and run the below steps 
+	1. dotnet restore (Make sure .NetCore 2.x is installed)
+	2. dotnet build (you can also build via VS2017)
+	3. dotnet publish
